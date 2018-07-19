d87dae3d13fad2fded331ac8802e0a7f86a35f97
maxwell <github email address>
2018-07-19 09:19:49 -0400

    added solution

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+          <div key={index}>
+              <h3>Name: {director.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {director.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 }
