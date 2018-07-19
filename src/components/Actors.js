d8056b0b2d3ca4d3d417839632f7cba9bba184b7
maxwell <github email address>
2018-07-19 09:19:49 -0400

    added solution

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+          <div key={index}>
+              <h3>Name: {actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {actor.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
