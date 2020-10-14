eacc7e88275e719f1cddc1f1ada342fba2a6b26d
Mattie43 <ericksenm93@ymail.com>
2020-10-14 16:13:30 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  function renderActors(){
+    return actors.map(actor => {
+      return (
+        <div>
+          <h3>Name: {actor.name}</h3>
+          Movies:
+          <ul>
+            {actor.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  function renderMovies() {
+    return movies.map(movie => {
+      return (
+        <div>
+          <h2>Title: {movie.title}</h2>
+          <h4>Time: {movie.time}</h4>
+          Genres:
+          <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
