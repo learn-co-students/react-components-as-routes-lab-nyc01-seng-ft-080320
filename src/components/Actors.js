f42a1a662a5453e05a7473bacbf24210481e3fde
kevinpark07 <kevin.park07@yahoo.com>
2020-10-14 16:48:57 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,20 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {
+          actors.map((actor,index) => (
+            <div className="actor" key={index}>
+              <h3>{actor.name}</h3>
+              <p>Movies: </p>
+              <ul>
+                {actor.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))}
+              </ul>
+            </div>
+          ))
+        }
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/actors" component={Actors} />
+      </div>
     </Router>
   );
 };
